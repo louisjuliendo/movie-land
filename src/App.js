4180270f67a38c6ba4a84d4aeb55dbf8f164caef
Louis Juliendo <louisjuliendo@gmail.com>
2020-06-11 21:42:10 +0700

    Add styling to the Search Movie component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import SearchMovie from "./searchMovie";
 
 function App() {
   return (
     <div className="container">
       <h1 className="title">Movie-Land</h1>
+      <SearchMovie />
     </div>
   );
 }
--- src/searchMovie.js
+++ src/searchMovie.js
@@ -1,16 +1,37 @@
 import React from "react";
 
 export default function searchMovie() {
+  const searchMovie = async (e) => {
+    e.preventDefault();
+    console.log("Submitting");
+
+    const query = "Jurassic park";
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=a8d37e967631e304efccdf4bc7f18cd0&language=en-US&query=${query}&page=1`;
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <form className="form">
-      <label className="label" htmlFor="query"></label>
+    <form className="form" onSubmit={searchMovie}>
+      <label className="label" htmlFor="query">
+        Movie Name
+      </label>
       <input
         className="input"
         type="text"
         name="query"
         placeholder="i.e Jurassic park"
       />
-      <button className="button" type="submit"></button>
+      <button className="button" type="submit">
+        Search
+      </button>
     </form>
   );
 }
